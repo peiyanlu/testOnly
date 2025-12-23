405a0cfdd180752741b8b62f7e2930fe9e11b900
YanluPei <peiyanlu@vip.qq.com>
2025-12-23 18:18:14 +0800

    chore(release): v1.0.2

--- _test_/tesp.ts
+++ _test_/tesp.ts
@@ -1,18 +1,23 @@
 import { readFileSync, writeFileSync } from 'node:fs'
 import { join } from 'node:path'
+import { format } from 'node:util'
 import semver from 'semver'
 import minimist from 'minimist'
 
 
-const { name, type } = minimist(process.argv.slice(2))
+// const { name, type } = minimist(process.argv.slice(2))
+//
+// const templatePath = join('templates', name, 'template.json')
+//
+// const tpl = JSON.parse(readFileSync(templatePath, 'utf8'))
+// tpl.version = semver.inc(tpl.version, type)!
+//
+// writeFileSync(templatePath, JSON.stringify(tpl, null, 2) + '\n')
+//
+// console.log(`✔ ${ name } bumped to ${ tpl.version }`)
 
-const templatePath = join('templates', name, 'template.json')
 
-const tpl = JSON.parse(readFileSync(templatePath, 'utf8'))
-tpl.version = semver.inc(tpl.version, type)!
+const context = Object.assign({}, { version: '*' });
+const match = format('${version}', context);
 
-writeFileSync(templatePath, JSON.stringify(tpl, null, 2) + '\n')
-
-console.log(`✔ ${ name } bumped to ${ tpl.version }`)
-
-
+console.log(match)
